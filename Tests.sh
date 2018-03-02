2b4d42d49a21664370708bb1f05d055fe892061a
Ben <b.richards@qmul.ac.uk>
2018-03-02 18:08:45 +0000

    teed output nd reduced tests

--- Tests.sh
+++ Tests.sh
@@ -151,7 +151,7 @@ do
 		
 		
 		cp $ValidationPath/Webpage/results.html $ValidationPath/Webpage/results.html.old
-		head -1000000 $ValidationPath/Webpage/results.html.old | sed s:${TRAVIS_COMMIT}"Pass"$1:$pass: | sed s:${TRAVIS_COMMIT}"Text"$i:$time: | sed s:${TRAVIS_COMMIT}"Link"$1:${TRAVIS_COMMIT}/log$1: > $ValidationPath/Webpage/results.html.new
+		head -1000000 $ValidationPath/Webpage/results.html.old | sed s:${TRAVIS_COMMIT}"Pass"$1:$pass: | sed s:${TRAVIS_COMMIT}"Text"$i:$time: | sed s:${TRAVIS_COMMIT}"Link"$1:${TRAVIS_COMMIT}/log$1: |& tee $ValidationPath/Webpage/results.html.new
 		
 	    fi	
 #############################################################
