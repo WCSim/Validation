129f55296729983fb40d7a747f1dfe98a174a1ba
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2018-10-16 16:25:18 +0100

    Make MakeReference.sh work without absolute paths

--- MakeReference.sh
+++ MakeReference.sh
@@ -1,19 +1,33 @@
 #! /bin/bash
 
-for mac in `ls /root/Validation/Generate/macReference/*.mac`
+#If you're not in docker, setup WCSim and then run with:
+# ./MakeReference.sh $PWD `which WCSim`
+
+cd $WCSIMDIR
+pwd
+WCSIMVALIDATIONDIR=${1:-/root/Validation/}
+
+#compile the plot-making macro
+g++ $WCSIMVALIDATIONDIR/Generate/daq_readfilemain.C -g3 -o $WCSIMVALIDATIONDIR/Generate/daq_readfilemain `root-config --libs --cflags` -L $WCSIMDIR -lWCSimRoot -I $WCSIMDIR/include
+
+#run WCSim
+for mac in `ls $WCSIMVALIDATIONDIR/Generate/macReference/*.mac`
 do
-echo $mac
-./exe/bin/Linux-g++/WCSim $mac
+    echo $mac
+    ${2:-./exe/bin/Linux-g++/WCSim} $mac
 done
 
+#generate plots
 for file in `ls *.root`
 do
-echo $file
-/root/Validation/Generate/daq_readfilemain $file
+    echo $file
+    $WCSIMVALIDATIONDIR/Generate/daq_readfilemain $file
+    sleep 5
 done
 
+#overwrite the reference plots
 for output in `ls ./analysed_*.root`
 do
-echo $output
-cp $output /root/Validation/Compare/Reference/
+    echo $output
+    cp $output $WCSIMVALIDATIONDIR/Compare/Reference/
 done
