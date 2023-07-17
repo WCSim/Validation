ebb602fb7d11ac9976c50a447749b1c1c3f9afc5
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2023-07-17 10:25:57 +0000

    Give 2nd required argument to daq_readfilemain

--- Generate/Generate.sh
+++ Generate/Generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 WCSim $1 $ValidationPath/Generate/macReference/tune/tuning_parameters.mac
-$ValidationPath/Generate/daq_readfilemain $2
+$ValidationPath/Generate/daq_readfilemain $2 0
