186c96ac82bd15af92d808998ff890cc9c37841b
WCSim CI <wcsim@wcsim.wcsim>
2023-12-12 11:16:18 +0000

    Fix typo

--- Generate/Generate.sh
+++ Generate/Generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 WCSim $1 $ValidationPath/Generate/macReference/tune/tuning_parameters.mac
-for pmtype in 0 1 2; do
+for pmttype in 0 1 2; do
     $ValidationPath/Generate/daq_readfilemain $2 0 $pmttype
 done
