a91ed8a9c713df8d24aa5ff634c7332624a0b331
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2024-03-20 11:17:44 +0000

    Update build.py
    Make subsequent builds faster with multithreading

--- build.py
+++ build.py
@@ -15,6 +15,8 @@ def main(option):
 
     cmake_command = f"cmake -DWCSim_DEBUG_COMPILE_FLAG=ON {extra_options} -DCMAKE_INSTALL_PREFIX=../install/ ../src/"
     make_command = "make"
+    if option == 1:
+        make_command += " -j`nproc`" 
     make_install_command = "make install"
 
     os.system(cmake_command)
@@ -23,6 +25,6 @@ def main(option):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Build and install script")
-    parser.add_argument("-o","--option", required=True,type=int, help="Option to pass to the script. Option 0 does nothing. Option 1 sets WCSim_Geometry_Overlaps_CHECK=ON", choices=list(range(2)))
+    parser.add_argument("-o","--option", required=True,type=int, help="Option to pass to the script. Option 0 does nothing. Option 1 sets WCSim_Geometry_Overlaps_CHECK=ON & compiles with multiple threads", choices=list(range(2)))
     args = parser.parse_args()
     main(args.option)
